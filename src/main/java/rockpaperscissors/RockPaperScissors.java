1a0d84010f4c49ae1495988e02251c55a8ae983e
Beth Hansen and Bart Webber <beth@>
2017-08-01 16:23:56 -0700

    add third test to display results

--- src/main/java/rockpaperscissors/RockPaperScissors.java
+++ src/main/java/rockpaperscissors/RockPaperScissors.java
@@ -11,9 +11,17 @@ public class RockPaperScissors {
 
 
 
-    public String runGame(String inputString){
-        return inputString;
+    public String runGame(String inputString1, String  inputString2){
+        String winningPlayer="";
+        if (inputString1.equals("paper") && inputString2.equals("rock")){
+            winningPlayer = "Player 1 is the winner!";
+        }
 
+
+
+
+
+        return winningPlayer;
     }
 
 }
--- src/test/java/rockpaperscissors/RockPaperScissorsTest.java
+++ src/test/java/rockpaperscissors/RockPaperScissorsTest.java
@@ -9,10 +9,17 @@ import static org.junit.Assert.*;
  */
 public class RockPaperScissorsTest {
 
+//    @Test
+//    public void computerChooses_shouldBeAString_true() throws Exception {
+//        RockPaperScissors testGame = new RockPaperScissors();
+//        String playerChoice = "scissors";
+//        assertEquals(playerChoice, testGame.runGame("scissors"));
+//    }
+
     @Test
-    public void computerChooses_shouldBeAString_true() throws Exception {
+    public void computerChooses_shouldBeAString2_true() throws Exception {
         RockPaperScissors testGame = new RockPaperScissors();
-        String playerChoice = "scissors";
-        assertEquals(playerChoice, testGame.runGame("scissors"));
+        String playerChoice = "Player 1 is the winner!";
+        assertEquals(playerChoice, testGame.runGame("paper", "rock"));
     }
 }
